c2027863bceebc8bf2b6157a12bf510af3d5a211
Danilo Salazar <danilo@origo.ec>
2023-07-12 12:44:16 -0400

    Update streamlit_app.py
    Updating the prompt to check whether we can include a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ openai_api_key = st.secrets["openai_api_key"]
 def generate_response(topic,business):
   llm = OpenAI(model_name='text-davinci-003', openai_api_key=openai_api_key)
   # Prompt
-  template = 'As an experienced contractor with the name of my business {business}, generate base one-page proposal that includes an introductory section, quote section and contact secction about {topic}.'
+  template = 'As an experienced contractor with the name of my business {business}, generate a base one-page proposal that includes an introductory section, quote section with a table to be filled out later, and contact section about {topic}.'
   prompt = PromptTemplate(input_variables=['topic','business'], template=template)
   prompt_query = prompt.format(topic=topic, business=business)
   # Run LLM model and print out response
